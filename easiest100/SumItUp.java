8bc43899d81d2bf20c93925d3dc2257b4f52e960
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2024-02-03 18:00:24 +0100

    add SumItUp solution

---
+++ easiest100/SumItUp.java
@@ -0,0 +1,26 @@
+package easiest100;
+
+public class SumItUp {
+    public int sum(int N) {
+        int t = N, sum = 0;
+        while (N > 0){
+            sum += N%10;
+            N /= 10;
+            if (N == 0){
+                if (sum > 9) {
+                    N = sum;
+                    sum = 0;
+                }else{
+                    return sum;
+                }
+            }
+        }
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        SumItUp s = new SumItUp();
+        System.out.println(s.sum(56452));
+    }
+}
