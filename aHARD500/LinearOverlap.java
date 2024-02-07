10a1eacd579f3f8d41456d43908afed31e7037e6
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2024-02-07 19:10:11 +0100

    Modify linearOverlap solution

--- aHARD500/LinearOverlap.java
+++ aHARD500/LinearOverlap.java
@@ -1,15 +1,15 @@
 package aHARD500;
 
-import	java.util.Arrays;
+import java.util.Arrays;
 
 public class LinearOverlap {
 
-    int [][]dp = new int[1<<16][16];
+    int[][] dp = new int[1 << 16][16];
 
-    String get(String a, String b){
+    String get(String a, String b) {
         StringBuilder x = new StringBuilder(a);
-        for (int i=Math.min(b.length(), a.length()); i>=1; i--){
-            String subs = b.substring(0,i);
+        for (int i = Math.min(b.length(), a.length()); i >= 1; i--) {
+            String subs = b.substring(0, i);
             if (a.endsWith(subs)) {
                 x.append(b.substring(i));
                 return x.toString();
@@ -19,29 +19,31 @@ public class LinearOverlap {
         return x.toString();
     }
 
-    int res(int mask, int ind, String mom, String []words){
-        if (mask == ((1<<words.length)-1)) return 0;
-        if (dp[mask][ind] != -1) return dp[mask][ind];
+    int res(int mask, int ind, String mom, String[] words) {
+        if (mask == ((1 << words.length) - 1))
+            return 0;
+        if (dp[mask][ind] != -1)
+            return dp[mask][ind];
 
-        int rez = 1<<30;
+        int rez = 1 << 30;
 
-        for (int i=0; i<words.length; i++)
-            if ((mask &(1<<i)) == 0) {
+        for (int i = 0; i < words.length; i++)
+            if ((mask & (1 << i)) == 0) {
                 String s = get(mom, words[i]);
-                rez = Math.min(rez, s.length()-mom.length() + res(mask | (1<<i), i, s, words));
+                rez = Math.min(rez, s.length() - mom.length() + res(mask | (1 << i), i, s, words));
             }
 
         return dp[mask][ind] = rez;
     }
 
     public int getMinString(String[] words) {
-        for (int i=0; i<(1<<16); i++)
+        for (int i = 0; i < (1 << 16); i++)
             Arrays.fill(dp[i], -1);
-        return res(0,15,"", words);
+        return res(0, 15, "", words);
     }
 
-   public static void main(String[] args) {
-       LinearOverlap l = new LinearOverlap();
-       System.out.println(l.getMinString(new String[]{"abc", "def", "ghi", "jkl", "mno"}));
-   }
+    public static void main(String[] args) {
+        LinearOverlap l = new LinearOverlap();
+        System.out.println(l.getMinString(new String[] { "abc", "def", "ghi", "jkl", "mno" }));
+    }
 }
